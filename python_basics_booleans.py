c69e9fd60bbaa118df889deccfe6a6048ab9febd
Francisco Becerril <fbecerril@DESKTOP-M9A8C37.localdomain>
2021-10-06 14:51:38 -0700

    added booleans file

---
+++ python_basics_booleans.py
@@ -0,0 +1,20 @@
+#Boolen Values
+print("10 > 9 = ", 10 > 9)
+print("10 == 9 = ", 10 == 9)
+print("10 < 9 = ", 10 < 9)
+
+#if using true or false
+print("\nif using true or false")
+a = 200
+b = 33
+print("a = 200 and b = 33")
+if(b > a):
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+#Evaluate Values and Variables
+print("\nEvaluate Values and Variables")
+
+print('bool("Hello") = ', bool("Hello"))
+print('bool(15) = ', bool(15))
