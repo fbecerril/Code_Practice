5968982321022d79a43cbd89974d33bf529e9494
Francisco Becerril <fbecerril@DESKTOP-M9A8C37.localdomain>
2021-10-07 01:39:32 -0700

    added python lists

---
+++ python_basics_lists.py
@@ -0,0 +1,15 @@
+#Lists
+thislist = ["apple", "banana", "cherry"]
+print("thislist = ", thislist)
+
+#lists can have duplicates
+#there are methods for ordering lists
+#we can use len() function to see the lenght
+
+print("len(thislists) = ", len(thislist))
+#lists can be integers, booleans, strings or a mix of the three
+print("\nlists can be integers, booleans, strings or a mix of the three")
+print("\nWe can use indexing to access lists")
+print("thislist[1] = ", thislist[1])
+print("thislist[-1] = ", thislist[-1])
+print("thislist[2:5] = ", thislist[2:5] )
